de7780a660539c5a47fcf5a3320c30dfaa0562f4
MalkielApolonio <malkielivanapolonio@gmail.com>
2018-02-05 21:01:49 -0800

    Sample Code
    AngularJS sample code

---
+++ album store/js/controller/mainController.js
@@ -0,0 +1,57 @@
+var app = angular.module("myApp", [])
+.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'Album Sazxclkle';}])
+  // $scope.promo = 'Featured Albums'
+  // $scope.products = [
+  //   {
+  //     name: 'Red Rose',
+  //     artist: 'Daniel Armstrong',
+  //     albumCover: '',
+  //     relDate: new Date('1998', '04', '02'),
+  //     price: '15.99'
+  //   },
+  //   {
+  //     name: 'Front Page',
+  //     artist: 'Daniel Croxe',
+  //     albumCover: '',
+  //     relDate: new Date('2015', '23', '03'),
+  //     price: '16.99'
+  //   },
+  //   {
+  //     name: 'Soul Soup',
+  //     artist: 'Tara Clare',
+  //     albumCover:'',
+  //     relDate: new Date('1995', '13', '02'),
+  //     price: '14.99'
+  //   },
+  //   {
+  //     name: 'Fallen Angel',
+  //     artist: '',
+  //     albumCover:'',
+  //     relDate: new Date('1999', '06', '15'),
+  //     price: '14.99'
+  //   },
+  //   {
+  //     name: 'Between',
+  //     artist: 'Ben Santorin',
+  //     albumCover:'',
+  //     relDate: new Date('2010', '04','25'),
+  //     price: '14.99'
+  //   },
+  //   {
+  //     name:'groov',
+  //     artist: '',
+  //     albumCover: '',
+  //     relDate: new Date ('1878', '07','17'),
+  //     price:'10.99'
+  //   },
+  //   {
+  //     name: 'Lamp Light',
+  //     artist: '',
+  //     albumCover: '',
+  //     relDate: new Date('1967', '10', '25'),
+  //     price: '11.99'
+  //   }
+  // ]
+// }]);
+//
